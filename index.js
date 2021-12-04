19f50163cd975615964d1cfa7ecc1945eac14818
Christopher <christopher.black.2209@googlemail.com>
2021-12-04 01:08:12 +0000

    commit tree bf7373fbb7e749ce4795e78ae3f46c5b676eef5b

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    let result = [];
+    for (const element of sourceArray) {
+        result.push(-1 * element)
+    }
+    return result
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+    let result = [];
+    for (const element of sourceArray) {
+        result.push(element*2)
+    }
+    return result
+}
+
+function mapToSquare(sourceArray) {
+    let result = [];
+    for (const element of sourceArray) {
+        result.push(element**2)
+    }
+    return result
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    let sum = startingPoint;
+    for (const element of sourceArray) {
+        sum += element
+    }
+    return sum
+}
+
+function reduceToAllTrue(sourceArray) {
+    let result = false;
+    for (const element of sourceArray) {
+        if (!!element === true) {
+            result = true
+        }
+        else {
+            result = false
+        }
+    }
+    return result
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let result = false;
+    for (const element of sourceArray) {
+        if (!!element === true) {
+            result = true
+        }
+    }
+    return result
+}
